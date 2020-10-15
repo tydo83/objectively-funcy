e48712e20513c9926536fafafb55ccbf6358e07b
Timothy Do <tydo83@gmail.com>
2020-10-15 13:08:50 -0400

    done

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 // Your code here.
+const getFirstName = function(person) {
+  return person.firstName;
+}
 
+const getLastName = function(person) {
+  return person.lastName;
+}
 
+const getFullName = function(person) {
+  return person.firstName + ' ' + person.lastName
+}
 
+const setFirstName = function(person, newFirstName) {
+  person.firstName = newFirstName; 
+}
 
+const setAge = function(person, age) {
+  person.age = age;
+}
+
+const giveBirthday = function(person) {
+  if('age' in person === false) {
+    person.age = 1;
+  }
+  else {
+    person.age +=1;
+  }
+}
+
+const marry = function(person1, person2) {
+  person1.spouseName = getFullName(person2);
+  person2.spouseName = getFullName(person1);
+  person1.married = true;
+  person2.married = true;
+}
+
+const divorce = function(person1, person2) {
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName;
+  delete person2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
